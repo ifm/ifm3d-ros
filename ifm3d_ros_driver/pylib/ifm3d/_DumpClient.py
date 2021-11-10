d5d19013e43824718f20e409b579f3265d0d076f
ifm-csr <support.robotics@ifm.com>
2021-11-10 10:00:47 +0000

    add latest dev changes and fixes

---
+++ ifm3d_ros_driver/pylib/ifm3d/_DumpClient.py
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2021 ifm electronic, gmbh
+
+"""
+Mimics the `ifm3d dump` command but communicates through the ROS graph
+"""
+
+import json
+import rospy
+import sys
+from ifm3d.srv import Dump
+
+SRV_TIMEOUT = 2.0 # seconds
+SRV_NAME = "/ifm3d/camera/Dump"
+
+class DumpClient(object):
+
+    def __init__(self):
+        rospy.init_node('ifm3d_dump_client')
+
+    def run(self):
+        rospy.wait_for_service(SRV_NAME, timeout=SRV_TIMEOUT)
+        proxy = rospy.ServiceProxy(SRV_NAME, Dump)
+        resp = proxy()
+        if resp.status == 0:
+            print(json.dumps(json.loads(resp.config),
+                             sort_keys=True, indent=4, separators=(',', ': ')))
+        else:
+            sys.stderr.write("Dump failed with error: %s - %s\n" %
+                             (str(resp.status),
+                              "Check the `ifm3d' logs for  more detail"))
+
+        return resp.status
---
+++ ifm3d_ros_driver/pylib/ifm3d/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2021 ifm electronic, gmbh
+
+from ._DumpClient import DumpClient
+from ._ConfigClient import ConfigClient
