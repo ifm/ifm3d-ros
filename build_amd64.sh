4348fdaf3d4f39a09a7a064ef259bd12d33c2d43
Philipp Senger <philipp.senger@ifm.com>
2023-03-17 17:46:04 +0100

    update build tag and remove GL build option

--- build_amd64.sh
+++ build_amd64.sh
@@ -6,25 +6,9 @@ IFM3D_CLONE_REPO=https://github.com/ifm/ifm3d
 IFM3D_ROS_CLONE_REPO=https://github.com/ifm/ifm3d-ros
 BASE_IMAGE_TAG=noetic-perception
 BASE_IMAGE=ros
-TAG=ifm3d-ros:noetic-x86_64
+TAG=ifm3d-ros:noetic-x86_64_new
 
 # docker build -t $TAG --no-cache --rm --build-arg IFM3D_CLONE_REPO=$IFM3D_CLONE_REPO --build-arg IFM3D_ROS_CLONE_REPO=$IFM3D_ROS_CLONE_REPO  \
     # --build-arg BASE_IMAGE=$BASE_IMAGE --build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG -f Dockerfile .
 docker build -t $TAG --build-arg IFM3D_CLONE_REPO=$IFM3D_CLONE_REPO --build-arg IFM3D_ROS_CLONE_REPO=$IFM3D_ROS_CLONE_REPO  \
     --build-arg BASE_IMAGE=$BASE_IMAGE --build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG -f Dockerfile .
-
-
-
-# GL
-# user_name=$1
-# token=$2
-# IFM3D_CLONE_REPO=https://$user_name:$token@gitlab.dev.ifm/syntron/support/csr/ifm3d/ifm3d
-# IFM3D_ROS_CLONE_REPO=https://$user_name:$token@gitlab.dev.ifm/syntron/support/csr/ifm3d/ifm3d-ros
-# BASE_IMAGE_TAG=noetic-perception
-# BASE_IMAGE=ros
-# TAG=ifm3d-ros:noetic-x86_64
-
-# docker build -t $TAG --no-cache --rm --build-arg IFM3D_CLONE_REPO=$IFM3D_CLONE_REPO --build-arg IFM3D_ROS_CLONE_REPO=$IFM3D_ROS_CLONE_REPO  \
-#     --build-arg BASE_IMAGE=$BASE_IMAGE --build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG -f Dockerfile .
-# docker build -t $TAG --build-arg IFM3D_CLONE_REPO=$IFM3D_CLONE_REPO --build-arg IFM3D_ROS_CLONE_REPO=$IFM3D_ROS_CLONE_REPO  \
-#     --build-arg BASE_IMAGE=$BASE_IMAGE --build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG -f Dockerfile .
