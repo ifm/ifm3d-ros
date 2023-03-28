ac60272dd5818a6ff1eb3734aa47a62c8ff0be1c
Philipp Senger <philipp.senger@ifm.com>
2023-03-28 11:56:57 +0200

    update build helpers

--- build_amd64.sh
+++ build_amd64.sh
@@ -6,9 +6,7 @@ IFM3D_CLONE_REPO=https://github.com/ifm/ifm3d
 IFM3D_ROS_CLONE_REPO=https://github.com/ifm/ifm3d-ros
 BASE_IMAGE_TAG=noetic-perception
 BASE_IMAGE=ros
-TAG=ifm3d-ros:noetic-x86_64_new
+TAG=ifm3d-ros:noetic-x86-64-v123
 
-# docker build -t $TAG --no-cache --rm --build-arg IFM3D_CLONE_REPO=$IFM3D_CLONE_REPO --build-arg IFM3D_ROS_CLONE_REPO=$IFM3D_ROS_CLONE_REPO  \
-    # --build-arg BASE_IMAGE=$BASE_IMAGE --build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG -f Dockerfile .
 docker build -t $TAG --build-arg IFM3D_CLONE_REPO=$IFM3D_CLONE_REPO --build-arg IFM3D_ROS_CLONE_REPO=$IFM3D_ROS_CLONE_REPO  \
     --build-arg BASE_IMAGE=$BASE_IMAGE --build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG -f Dockerfile .
--- build_arm64.sh
+++ build_arm64.sh
@@ -6,26 +6,7 @@ IFM3D_CLONE_REPO=https://github.com/ifm/ifm3d
 IFM3D_ROS_CLONE_REPO=https://github.com/ifm/ifm3d-ros
 BASE_IMAGE_TAG=noetic-perception
 BASE_IMAGE=arm64v8/ros
-TAG=ifm3d-ros:noetic-arm64v8
-
-# docker build -t ifm3d-ros:noetic-arm64v8 --no-cache --rm --build-arg IFM3D_CLONE_REPO=$IFM3D_CLONE_REPO --build-arg IFM3D_ROS_CLONE_REPO=$IFM3D_ROS_CLONE_REPO  \
-#     --build-arg BASE_IMAGE=ros --build-arg BASE_IMAGE_TAG=noetic-perception -f Dockerfile .
+TAG=ifm3d-ros:noetic-arm64v8-v123
 
 docker build -t $TAG --build-arg IFM3D_CLONE_REPO=$IFM3D_CLONE_REPO --build-arg IFM3D_ROS_CLONE_REPO=$IFM3D_ROS_CLONE_REPO  \
     --build-arg BASE_IMAGE=$BASE_IMAGE --build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG -f Dockerfile .
-
-
-# GL: arm64
-# user_name=$1
-# token=$2
-# IFM3D_CLONE_REPO=https://$user_name:$token@gitlab.dev.ifm/syntron/support/csr/ifm3d/ifm3d
-# IFM3D_ROS_CLONE_REPO=https://$user_name:$token@gitlab.dev.ifm/syntron/support/csr/ifm3d/ifm3d-ros
-# BASE_IMAGE_TAG=noetic-perception
-# BASE_IMAGE=arm64v8/ros
-# TAG=ifm3d-ros:noetic-arm64v8
-
-# docker build -t ifm3d-ros:noetic-arm64v8 --no-cache --rm --build-arg IFM3D_CLONE_REPO=$IFM3D_CLONE_REPO --build-arg IFM3D_ROS_CLONE_REPO=$IFM3D_ROS_CLONE_REPO  \
-#     --build-arg BASE_IMAGE=ros --build-arg BASE_IMAGE_TAG=noetic-perception -f Dockerfile .
-
-# docker build -t $TAG --build-arg IFM3D_CLONE_REPO=$IFM3D_CLONE_REPO --build-arg IFM3D_ROS_CLONE_REPO=$IFM3D_ROS_CLONE_REPO  \
-#     --build-arg BASE_IMAGE=$BASE_IMAGE --build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG -f Dockerfile .
