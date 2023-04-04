583b45b12a7bacea5e9e407c342b2e66d5a2d57e
Philipp Senger <philipp.senger@ifm.com>
2023-04-04 10:10:36 +0200

    remove unused build args

--- build_arm64.sh
+++ build_arm64.sh
@@ -8,5 +8,5 @@ BASE_IMAGE_TAG=noetic-perception
 BASE_IMAGE=arm64v8/ros
 TAG=ifm3d-ros:noetic-arm64v8-v123
 
-docker build -t $TAG --build-arg IFM3D_CLONE_REPO=$IFM3D_CLONE_REPO --build-arg IFM3D_ROS_CLONE_REPO=$IFM3D_ROS_CLONE_REPO  \
+docker build -t $TAG --build-arg IFM3D_CLONE_REPO=$IFM3D_CLONE_REPO \
     --build-arg BASE_IMAGE=$BASE_IMAGE --build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG -f Dockerfile .
